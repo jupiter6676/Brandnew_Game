1abc9fd2e5106f6a56e030b5b92d69ba5d922a6f
HOJIE039 <tlsgpwls311@gmail.com>
2023-01-07 23:01:01 +0900

    230107

---
+++ Brandnew Game/BN/Assets/Scripts/ParallaxCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+/* CameraController.cs로 대체 예정 */
+public class ParallaxCamera : MonoBehaviour
+{
+    public delegate void ParallaxCameraDelegate(float deltaMovement);
+    public ParallaxCameraDelegate onCameraTranslate;
+    private float oldPosition;
+    
+    void Start()
+    {
+        oldPosition = transform.position.x; // 시작 시 oldPosition = 현재 x 좌표
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+    void FixedUpdate()
+    {
+        if (transform.position.x != oldPosition)    // 플레이어가 이동
+        {
+            if (onCameraTranslate != null)
+            {
+                // delta = 이전 위치 - 현재 위치
+                float delta = oldPosition - transform.position.x;
+                onCameraTranslate(delta);   // delta만큼 카메라 이동
+            }
+
+            oldPosition = transform.position.x; // oldPosition을 현재 위치로 초기화
+        }
+    }
+}
